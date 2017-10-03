d306778fe4c51e9319e0325b5e9c23feff6379f1
Jesse Atay <jesse.atay@gmail.com>
2017-10-03 06:07:08 -0700

    update json and gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require("gulp");
+var watch = require("gulp-watch");
 
 gulp.task("default", function(){
   console.log("test gulp");
@@ -6,4 +7,10 @@ gulp.task("default", function(){
 
 gulp.task("html", function(){
   console.log("test type 2");
+});
+
+gulp.task("watch", function(){
+  watch(".app/index/html", function(){
+    gulp.start("html");
+  });
 });
