48bf9dd6a46d31ec26dfeaafa0f49e1af55dd352
Jesse Atay <jesse.atay@gmail.com>
2017-10-03 06:17:42 -0700

    update gulp, add css file

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,18 @@ gulp.task("html", function(){
   console.log("test type 2");
 });
 
+gulp.task("styles", function(){
+  console.log("imagine sass or css tasks running here");
+});
+
 gulp.task("watch", function(){
   watch(".app/index.html", function(){
     gulp.start("html");
+  });
+});
+
+gulp.task("watch", function(){
+  watch(".app/assets/styles/**/*.css", function(){
+    gulp.start("styles");
   });
 });
