c84207b23080f559aaddf42faf14dc2e656bf23b
Jesse Atay <jesse.atay@gmail.com>
2017-10-04 04:33:22 -0700

    na

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function() {
-  console.log("Gulp default task");
-})
+  console.log("Hooray - you created a Gulp task.");
+});
 
 gulp.task('html', function() {
-  console.log("Gulp html task");
-})
+  console.log("Imagine something useful being done to your HTML here.");
+});
 
-gulp.task('styles', function(){
-return gulp.src('./app/assets/styles/styles.css')
-.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-.pipe(gulp.dest('./app/temp/styles'));
-})
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
 
 gulp.task('watch', function() {
+
   watch('./app/index.html', function() {
     gulp.start('html');
-  })
-  watch('./app/assets/styles/**/*.css', function(){
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('styles');
-  })
-})
+  });
+
+});
